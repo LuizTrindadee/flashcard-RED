899302f2c1ced18820fe2b144820bba8509208ab
LuizTrindadee <luizotaviojau08@gmail.com>
2024-11-27 09:08:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Red Dead Redemption 2',
+    'Um dos melhores jogos de história!',
+    'Estados Unidos, 1899. O fim da era do velho oeste se aproxima, e os xerifes caçam as últimas gangues fora da lei. Quem não se rende ou sucumbe, acaba morto. Depois de tudo dar errado em um roubo na cidade de Blackwater, Arthur Morgan e a gangue Van der Linde são obrigados a fugir.'
+)
+
+criaCartao(
+    'Playstation 5 ou XboxSeries X',
+    'Qual é melhor?',
+    'O Playstation 5 tem melhor desempenho que o Xbox Series X usando o blu-ray, disco, no qual é gravado a maioria dos jogos exclusivos. Já o console da Microsoft supera o da Sony no streaming.'
+)
+
+criaCartao(
+    'Futebol é bom de mais',
+    'Qual fifa é dito como melhor pelo público?',
+    'FIFA 14, considerado como o título favorito de muitos fãs, foi lançado com a engine Ignite, que trouxe mais naturalidade para os gráficos, movimento dos jogadores e para a gameplay em geral.' 
+)
+
+criaCartao(
+    'Terror',
+    'Outlast 2 é o terror puro mesmo?',
+    'Uns dos melhores jogos de terror, Outlast 2 traz Sullivan Knoth e seus seguidores, que abandonaram o nosso mundo cruel para fundar Temple Gate, uma cidade nas profundezas das florestas e escondida da civilização. Knoth e o seu pessoal estão se preparando para o sofrimento do fim dos tempos e você está bem no meio disso.'
+)
+
